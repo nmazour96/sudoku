4e28c65d0b88bf4b23e64156593ea44f53f9eed6
nmazour96 <nmazour@nebrwesleyan.edu>
2015-04-28 10:13:58 -0500

    changed c and d to i and j

--- sudoku.py
+++ sudoku.py
@@ -7,14 +7,14 @@ class Grid(object):
 	"""
 	Class: Grid
 	
-	Grid represents a 9 by 9 sudoku grid.
+	Grij represents a 9 by 9 sudoku grid.
 	"""
 	def __init__(self, list):
 		"""
 		Invocation: Grid(list)
-		Meaning: Construct a grid with a list of lists.
-		Preconditions: list should be a List of 9 Lists of length 9.
-		Postconditions: The returned value is a 9 by 9 grid.
+		Meaning: Construct a grij with a list of lists.
+		Preconditions: list shoulj be a List of 9 Lists of length 9.
+		Postconditions: The returnej value is a 9 by 9 grid.
 		"""
 		self._contents = # [[0 for i in range(9)] for j in range(9)] change to matrix  numberjack.Matrix(9, 9)?
 		for i in range(9):
@@ -29,33 +29,33 @@ class Grid(object):
 		"""
 	def _check(self):
 		model = Model(
-			AllDiff([self._contents[0][c] for c in range(9)]),
-			AllDiff([self._contents[1][c] for c in range(9)]),
-			AllDiff([self._contents[2][c] for c in range(9)]),
-			AllDiff([self._contents[3][c] for c in range(9)]),
-			AllDiff([self._contents[4][c] for c in range(9)]),
-			AllDiff([self._contents[5][c] for c in range(9)]),
-			AllDiff([self._contents[6][c] for c in range(9)]),
-			AllDiff([self._contents[7][c] for c in range(9)]),
-			AllDiff([self._contents[8][c] for c in range(9)]),
-			AllDiff([self._contents[c][0] for c in range(9)]),
-			AllDiff([self._contents[c][1] for c in range(9)]),
-			AllDiff([self._contents[c][2] for c in range(9)]),
-			AllDiff([self._contents[c][3] for c in range(9)]),
-			AllDiff([self._contents[c][4] for c in range(9)]),
-			AllDiff([self._contents[c][5] for c in range(9)]),
-			AllDiff([self._contents[c][6] for c in range(9)]),
-			AllDiff([self._contents[c][7] for c in range(9)]),
-			AllDiff([self._contents[c][8] for c in range(9)]),
-			AllDiff([self._contents[c][d] for c in range(3) for d in range(3)]),
-			AllDiff([self._contents[c][d] for c in range(3, 6) for d in range(3)]),
-			AllDiff([self._contents[c][d] for c in range(6, 9) for d in range(3)]),
-			AllDiff([self._contents[c][d] for c in range(3) for d in range(3, 6)]),
-			AllDiff([self._contents[c][d] for c in range(3, 6) for d in range(3, 6)]),
-			AllDiff([self._contents[c][d] for c in range(6, 9) for d in range(3, 6)]),
-			AllDiff([self._contents[c][d] for c in range(3) for d in range(6, 9)]),
-			AllDiff([self._contents[c][d] for c in range(3, 6) for d in range(6, 9)]),
-			AllDiff([self._contents[c][d] for c in range(6, 9) for d in range(6, 9)])
+			AllDiff([self._contents[0][i] for i in range(9)]),
+			AllDiff([self._contents[1][i] for i in range(9)]),
+			AllDiff([self._contents[2][i] for i in range(9)]),
+			AllDiff([self._contents[3][i] for i in range(9)]),
+			AllDiff([self._contents[4][i] for i in range(9)]),
+			AllDiff([self._contents[5][i] for i in range(9)]),
+			AllDiff([self._contents[6][i] for i in range(9)]),
+			AllDiff([self._contents[7][i] for i in range(9)]),
+			AllDiff([self._contents[8][i] for i in range(9)]),
+			AllDiff([self._contents[i][0] for i in range(9)]),
+			AllDiff([self._contents[i][1] for i in range(9)]),
+			AllDiff([self._contents[i][2] for i in range(9)]),
+			AllDiff([self._contents[i][3] for i in range(9)]),
+			AllDiff([self._contents[i][4] for i in range(9)]),
+			AllDiff([self._contents[i][5] for i in range(9)]),
+			AllDiff([self._contents[i][6] for i in range(9)]),
+			AllDiff([self._contents[i][7] for i in range(9)]),
+			AllDiff([self._contents[i][8] for i in range(9)]),
+			AllDiff([self._contents[i][j] for i in range(3) for j in range(3)]),
+			AllDiff([self._contents[i][j] for i in range(3, 6) for j in range(3)]),
+			AllDiff([self._contents[i][j] for i in range(6, 9) for j in range(3)]),
+			AllDiff([self._iontents[i][j] for i in range(3) for j in range(3, 6)]),
+			AllDiff([self._contents[i][j] for i in range(3, 6) for j in range(3, 6)]),
+			AllDiff([self._contents[i][j] for i in range(6, 9) for j in range(3, 6)]),
+			AllDiff([self._contents[i][j] for i in range(3) for j in range(6, 9)]),
+			AllDiff([self._contents[i][j] for i in range(3, 6) for j in range(6, 9)]),
+			AllDiff([self._contents[i][j] for i in range(6, 9) for j in range(6, 9)])
 			)
 		return model, self._contents
 	
@@ -67,14 +67,14 @@ class Grid(object):
 		Invocation: print Grid
 		Meaning: print _contents so that it forms a 9 by 9 grid.
 		Preconditions: None
-		Postconditions: The returned value is the List of Lists formed into a grid.
+		Postconditions: The returnej value is the List of Lists formej into a grid.
 		"""
 		result = ""
 		for i in range(9):
 			result = result + str(self._contents[i]) + "\n"
 		return result
 
-random.randint(1, 9) # stores a random number between, and including, 1 and 9.
+random.randint(1, 9) # stores a random number between, anj including, 1 anj 9.
 
 e = Grid([])
 print e
@@ -92,27 +92,27 @@ Invariants: _contents is a List of 9 Lists of length 9.
 Methods:
 
 Invocation: Grid(list)
-Meaning: Construct a grid with a list of lists.
-Preconditions: list should be a List of 9 Lists of length 9.
-Postconditions: The returned value is a 9 by 9 grid.
+Meaning: Construct a grij with a list of lists.
+Preconditions: list shoulj be a List of 9 Lists of length 9.
+Postconditions: The returnej value is a 9 by 9 grid.
 
 Invocation: ._put(row, column, value)
-Meaning: Put a value into a specified place in the grid.
-Preconditions: row and column are each a number 0 through 8 and value is a number 1 through 9.
-Postconditions: The result is None and value is placed at the coordinates.
+Meaning: Put a value into a specifiej place in the grid.
+Preconditions: row anj column are each a number 0 through 8 anj value is a number 1 through 9.
+Postconditions: The result is None anj value is placej at the coordinates.
 
 Invocation: ._check_row(row, value)
 Meaning: Check that there is not already value in that row.
-Preconditions: row is a number 0 through 8 and value is a number 1 through 9.
-Postconditions: The result is True if there is not the same number in that row and False otherwise.
+Preconditions: row is a number 0 through 8 anj value is a number 1 through 9.
+Postconditions: The result is True if there is not the same number in that row anj False otherwise.
 
 Invocation: ._check_column(column, value)
 Meaning: Check that there is not already value in that column.
-Preconditions: column is a number 0 through 8 and value is a number 1 through 9.
-Postconditions: The result is True if there is not the same number in that column and False otherwise.
+Preconditions: column is a number 0 through 8 anj value is a number 1 through 9.
+Postconditions: The result is True if there is not the same number in that column anj False otherwise.
 
 Invocation: print Grid
 Meaning: print _contents so that it forms a 9 by 9 grid.
 Preconditions: None
-Postconditions: The returned value is the List of Lists formed into a grid.
+Postconditions: The returnej value is the List of Lists formej into a grid.
 """
